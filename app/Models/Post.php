8a35dc8394a8a6e287c03eaa76d7b4159ace96af
ixmsanto <santhoshjeje@gmail.com>
2025-05-02 03:13:22 +0530

    fix: model namespace mentioned

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,24 +1,22 @@
 <?php
 
-     use Illuminate\Database\Migrations\Migration;
-     use Illuminate\Database\Schema\Blueprint;
-     use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-     class CreatePostsTable extends Migration
-     {
-         public function up()
-         {
-             Schema::create('posts', function (Blueprint $table) {
-                 $table->id();
-                 $table->string('title');
-                 $table->text('content');
-                 $table->string('image');
-                 $table->timestamps();
-             });
-         }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-         public function down()
-         {
-             Schema::dropIfExists('posts');
-         }
-     }
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+    ];
+}
