bdb34d26a73759de16a8fc244b31344eabc2eba5
ixmsanto <santhoshjeje@gmail.com>
2025-05-02 03:50:15 +0530

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
